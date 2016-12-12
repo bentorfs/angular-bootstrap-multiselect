4a8b09b0b2144e5e9457b61082af2a649e350dc6
Samuel Santos <samaxes@gmail.com>
2016-12-12 19:34:52 +0000

    Update NPM dependencies to the last versions
    Also fix build errors

--- Gruntfile.js
+++ Gruntfile.js
@@ -129,8 +129,8 @@ module.exports = function (grunt) {
 
         bump: {
             options: {
-                files: ['package.json', 'bower.json'],
-                commitFiles: ['package.json', 'bower.json'],
+                files: ['package.json'],
+                commitFiles: ['package.json'],
                 tagName: '%VERSION%',
                 pushTo: 'origin'
             }
